2f304fecf0dca88e4fbf714036faca403c814613
sabari245 <sabari2004@hotmail.com>
2022-04-01 18:22:32 +0530

    added typescript; moved components to a separate folder

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  typescript: {
+    transpileOnly: true, // same as ts-node --transpile-only
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
